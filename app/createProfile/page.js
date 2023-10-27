5873792280764b83b41a8730630473da62024872
Finn Bledsoe <bledsoef@berea.edu>
2023-10-27 12:32:20 -0400

    working on select dropdown for referral pref

--- app/createProfile/page.js
+++ app/createProfile/page.js
@@ -1,14 +1,51 @@
+"use client";
+import { Menu } from "@headlessui/react"
+import Link from "next/link"
 export default function CreateProfile() {
     return (
-        <main>
+        <div>
             <input className="flex p-4 rounded text-xl text-black mr-4" placeholder="Full Name"/>
             <input type="text" placeholder="Email"/>
             <input type="text" placeholder="Position"/>
             <input type="text" placeholder="Company Name"/>
             <input type="text" placeholder="LinkedIn"/>
             <input type="text" placeholder="Graduation Year"/>
-            <input type="checkbox"/>
-
-        </main>
-        )
+            <select></select>
+            <input name="referralBox" type="checkbox"/>
+            <label for={"referralBox"} className="text-black">Open to giving referrals?</label>
+        </div>
+    )
 }
+
+// function ReferralDropdown() {
+//     return (
+//     <Menu>
+//       <Menu.Button className={"text-black"}>More</Menu.Button>
+//       <Menu.Items>
+//         <Menu.Item>
+//           {({ active }) => (
+//             <Link
+//               className={`${active && 'bg-blue-500'} text-black`}
+//               href="/account-settings"
+//             >
+//               Account settings
+//             </Link>
+//           )}
+//         </Menu.Item>
+//         <Menu.Item>
+//           {({ active }) => (
+//             <Link
+//               className={`${active && 'bg-blue-500'}`}
+//               href="/account-settings"
+//             >
+//               Documentation
+//             </Link>
+//           )}
+//         </Menu.Item>
+//         <Menu.Item disabled>
+//           <span className="opacity-75">Invite a friend (coming soon!)</span>
+//         </Menu.Item>
+//       </Menu.Items>
+//     </Menu>
+//     )
+// }
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,11 @@
 import Link from "next/link"
 export default function NavBar() {
     return (
-    <div className="w-full absolute top-0 bg-bereaBlue h-20 flex row-auto items-center text-white">
+    <div className="w-full bg-bereaBlue h-20 flex row-auto items-center text-white">
         <p className="self-center w-1/6 text-3xl m-8 font-semibold">BereaConnect</p>
         <div className="flex row-auto justify-end items-center h-full w-full">
-            <Link href={"/generate"} className="text-white text-2xl m-8 font-semibold">Find a Company</Link>
-            <Link href={"/submit"} className="text-white text-2xl m-8 font-semibold">My Profile</Link>
+            <Link href={"/findCompany"} className="text-white text-2xl m-8 font-semibold">Find a Company</Link>
+            <Link href={"/createProfile"} className="text-white text-2xl m-8 font-semibold">My Profile</Link>
 
         </div>
 </div>)
